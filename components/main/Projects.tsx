b81e0fe3362a225918ac6b5c879340e10e3c4499
Ridho_ferdiansa <100394990+ridhoF-1@users.noreply.github.com>
2023-12-08 14:43:42 +0700

    upload

---
+++ components/main/Projects.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ProjectCard from "../sub/ProjectCard";
+
+const Projects = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center py-20"
+      id="projects"
+    >
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        My Projects
+      </h1>
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+        <ProjectCard
+          src="/Diagnosis-Pintar.png"
+          title="Diagnosis Pintar"
+          description="Sistem Pakar
+          Kesehatan yang peduli
+          kesehatan Anda
+          Dapatkan hasil diagnosis yang akurat dan cepat dengan bantuan teknologi
+          machine learning."
+        />
+
+        <ProjectCard
+          src="/Space-Portfolio.png"
+          title="upcoming project"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <ProjectCard
+          src="/Space-Portfolio.png"
+          title="upcoming project"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
